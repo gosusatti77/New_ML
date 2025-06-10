50a7ef77a9e010b0965ee34766bf0e58c8278f90
Satish Gosu <gosusatti77@gmail.com>
2025-06-10 15:26:00 +0530

    updated requirements

--- setup.py
+++ setup.py
@@ -22,6 +22,6 @@ version='0.0.1',
 author='Satish Kumar',
 author_email='gosusatish.iitk@gmail.com',
 packages=find_packages(),
-install_requires=get_requirements('requirements-3.9.txt')
+install_requires=get_requirements('requirements.txt')
 
 )
